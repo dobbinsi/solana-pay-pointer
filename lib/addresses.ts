c54959623e4630c2369403aea1c08bdfeb9a0a5d
dobbinsi <iandobbins5@gmail.com>
2022-07-30 22:22:23 -0700

    to deploy on devnet

---
+++ lib/addresses.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('43AbhctQ6fpNLQ5GCfAMtWtTTcTxHfGtRdTMLejzNb8L') 
+
+// Devnet USDC address
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
